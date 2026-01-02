b0cd2a8e76caee2d93c45e0362da0cfe2aca54d5
EJ Fox <ejfox@ejfox.com>
2026-01-01 23:43:12 -0500

    fix: stricter regex - require domain.tld format

--- grab-links.sh
+++ grab-links.sh
@@ -2,7 +2,7 @@
 # tmux-link-grab - URL seeking for tmux
 
 tmux capture-pane -pJ -S-100 2>/dev/null | \
-  grep -oE 'https?://[^[:space:]>"'\'')<]+' | \
+  grep -oE 'https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}[^[:space:]>"'\'')<]*' | \
   awk '!seen[$0]++' | \
   tac | \
   fzf --height=40% --reverse --header='↵ copy' | \
